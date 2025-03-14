8415872cf76baf8408437c485dce28ac6fecca4d
HarryGua <1872578022@qq.com>
2025-03-14 21:25:51 +1100

    更新服务器配置：移除静态文件服务，更新 MongoDB 连接字符串

--- todo/server/index.js
+++ todo/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // 导入路由
 const authRoutes = require('./routes/auth');
@@ -26,6 +27,18 @@ app.use('/api/pomodoros', pomodoroRoutes);
 // 根路由
 app.get('/', (req, res) => {
   res.send('Pomodoro API 正在运行');
+});
+
+// 注释掉静态文件服务
+// app.use(express.static(path.join(__dirname, '../todo/dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../todo/dist/index.html'));
+// });
+
+// 错误处理中间件
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: '服务器内部错误' });
 });
 
 // 连接到 MongoDB
